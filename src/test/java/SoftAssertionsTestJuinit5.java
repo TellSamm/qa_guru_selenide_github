58f7a28a7710aa2032b9c606cd53b998e3828c38
TellSamm <chernikov.semen21@gmail.com>
2023-02-22 17:26:16 +0300

    added first step

---
+++ src/test/java/SoftAssertionsTestJuinit5.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SoftAssertionsTestJuinit5 {
+
+    @BeforeAll
+    static void openPracticeForm(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+        open("https://github.com/search?q=selenide");
+        System.out.println("*Start test info!*");
+
+    }
+
+
+    @Test
+    void codeSearchJunit5(){
+        $("div.f4.text-normal a[href='/selenide/selenide']").click();
+    }
+
+
+
+
+
+
+}
