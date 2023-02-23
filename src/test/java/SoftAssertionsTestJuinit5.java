f5c48926a78993ec3a49fd135d4bbdd8f0f3daed
TellSamm <chernikov.semen21@gmail.com>
2023-02-23 23:30:04 +0300

    added demo2 test

--- src/test/java/SoftAssertionsTestJuinit5.java
+++ src/test/java/SoftAssertionsTestJuinit5.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -22,7 +23,8 @@ public class SoftAssertionsTestJuinit5 {
     void codeSearchJunit5(){
         $("div.f4.text-normal a[href='/selenide/selenide']").click();
         $("#wiki-tab").click();
-        $("button[type='button']").scrollIntoView(".f6.Link--muted.js-wiki-more-pages-link.btn-link.mx-auto").click();
+        $(".f6.Link--muted.js-wiki-more-pages-link.btn-link.mx-auto").scrollTo().click();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions")).click();
 
     }
 
